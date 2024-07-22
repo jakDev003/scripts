90c59933f3857588b36abc770f2be003f41c856c
Josh K <jkagiwada@advisor360.com>
2024-07-22 20:30:20 +0000

    Updated reset_get

--- Work_Scripts/reset_git.sh
+++ Work_Scripts/reset_git.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-for dir in ~/code/*
+for dir in /home/dev/code/*
 do
     dir=${dir%*/} # remove the trailing slash
     echo "${dir##*/}"
     cd ${dir}
     pwd
     git reset --hard
+    git clean -fd
+    git fetch
+    git pull origin master
     git checkout master
 done
