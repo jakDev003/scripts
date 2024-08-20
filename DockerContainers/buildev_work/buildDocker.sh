05cbec8006eb68b9683ba06222aca175c9d5099f
Josh K <jkagiwada@advisor360.com>
2024-08-20 21:05:47 +0000

    Updated Build Dev Work

--- DockerContainers/buildev_work/buildDocker.sh
+++ DockerContainers/buildev_work/buildDocker.sh
@@ -5,3 +5,12 @@ docker image rm jak/buildev
 
 #docker build -t jak/buildev .
 docker build -f Dockerfile.jak -t jak/buildev .
+
+CONTAINER_NETWORK=a360i
+
+DEVBUILD_USER=$(docker inspect --format='{{.Config.User}}' jak/buildev);        \
+docker run -d -it --name buildev --network $CONTAINER_NETWORK -h devbuild --network-alias devbuild   \
+	-e MAVEN_USERNAME=admin --rm -v "$(HOME)/home_dev:/home/$DEVBUILD_USER" -v "publi.vol:/publish" \
+	-v "trans.vol:/xfer" -v "codev.vol:/home/$${DEVBUILD_USER}/code" -p 1022:22                        \
+	jak/buildev bash
+
