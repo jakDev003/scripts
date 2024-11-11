ac009868e4463d2e115f4acec35c0ac53a97164d
Josh K <linkspace003@fastmail.com>
2024-11-11 14:59:35 -0500

    Update reset_git.sh

--- Work_Scripts/reset_git.sh
+++ Work_Scripts/reset_git.sh
@@ -3,12 +3,20 @@
 for dir in /home/dev/code/*
 do
     dir=${dir%*/} # remove the trailing slash
+    echo "========================================="
     echo "${dir##*/}"
+    echo "========================================="
     cd ${dir}
     pwd
-    git reset --hard
+    git merge --abort
+    git reset --hard HEAD
     git clean -fd
     git fetch
     git pull origin master
     git checkout master
+    echo "========================================="
+    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    echo "========================================="
+    echo ""
+    echo ""
 done
