cb96a3ffa47e6071e142d26c90c7eff0106ac11e
Josh K <jkagiwada@advisor360.com>
2024-12-12 21:35:48 -0500

    updated home directory to windows home directory

--- DockerContainers/buildev_work/buildDocker.ps1
+++ DockerContainers/buildev_work/buildDocker.ps1
@@ -34,7 +34,7 @@ if (docker images -q jak/bd) {
 
     $DEVBUILD_USER = $(docker inspect --format='{{.Config.User}}' jak/bd)
     docker run -d -it --name buildev2 --network $CONTAINER_NETWORK -h devbuild --network-alias devbuild `
-        -e MAVEN_USERNAME=admin --rm -v "/home/dev/home_dev:/home/$DEVBUILD_USER" -v "publi.vol:/publish" `
+        -e MAVEN_USERNAME=admin --rm -v "$HOME/home_dev:/home/$DEVBUILD_USER" -v "publi.vol:/publish" ` 
         -v "trans.vol:/xfer" -v "codev.vol:/home/$DEVBUILD_USER/code" -p 1023:22 `
         jak/bd bash
 } else {
