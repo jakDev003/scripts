b368bc57d364e7b4db5220710238c1079e71e320
Josh K <linkspace003@fastmail.com>
2025-01-03 23:40:51 -0500

    Update install_lazygit.sh

--- Personal_Scripts/install_lazygit.sh
+++ Personal_Scripts/install_lazygit.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-GOVERSION=1.19.5
-curl -OLk https://golang.org/dl/go$GOVERSION.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvf go$GOVERSION.linux-amd64.tar.gz
-source /home/dev/.bashrc
-export PATH=$PATH:/usr/local/go/bin \
-    && go version
-source ~/.bashrc
-go version
-sudo rm -rf go*
- 
-LAZYGIT_VERSION=$(curl -sk "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lok lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
-sudo rm -rf lazygit*
+sudo install lazygit /usr/local/bin
