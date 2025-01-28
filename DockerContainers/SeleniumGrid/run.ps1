b544bba6c8eb5199164fe6aeff2587804112bd35
Joshua Kagiwada <jkagiwada@advisor360.com>
2025-01-28 10:30:06 -0500

    commented out safari it is blocked

--- DockerContainers/SeleniumGrid/run.ps1
+++ DockerContainers/SeleniumGrid/run.ps1
@@ -2,6 +2,5 @@ docker pull --tls-verify=false selenium/hub
 docker pull --tls-verify=false selenium/node-chrome
 docker pull --tls-verify=false selenium/node-firefox
 docker pull --tls-verify=false selenium/node-opera
-docker pull --tls-verify=false selenium/node-safari
 docker pull --tls-verify=false selenium/node-edge
 docker compose up -d
