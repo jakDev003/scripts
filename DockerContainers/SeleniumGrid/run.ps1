c4545ada79e6a0b9ed19f04d018fb78ababdec03
Joshua Kagiwada <jkagiwada@advisor360.com>
2025-02-26 09:21:33 -0500

    updating run.ps1 to download docker images first and then run docker compose to bypass issues with windows

--- DockerContainers/SeleniumGrid/run.ps1
+++ DockerContainers/SeleniumGrid/run.ps1
@@ -1,6 +1,27 @@
-docker pull --tls-verify=false selenium/hub
-docker pull --tls-verify=false selenium/node-chrome
-docker pull --tls-verify=false selenium/node-firefox
-docker pull --tls-verify=false selenium/node-opera
-docker pull --tls-verify=false selenium/node-edge
-docker compose up -d
+# Define the images to be pulled
+$images = @(
+    "selenium/node-chromium:latest",
+    "selenium/node-firefox:latest",
+    "selenium/node-edge:latest",
+    "selenium/node-opera:latest",
+    "selenium/node-safari:latest",
+    "selenium/hub:latest"
+)
+
+# Pull the images if they are not already present
+foreach ($image in $images) {
+    if (-not (docker images -q $image)) {
+        Write-Host "   =====> Pulling image $image..."
+        docker pull --tls-verify=false $image
+    } else {
+        Write-Host "   =====> Image $image already exists."
+    }
+}
+
+# Run docker-compose down
+Write-Host "   =====> Running docker-compose down..."
+docker-compose down
+
+# Run docker-compose up with the specified options
+Write-Host "   =====> Running docker-compose up -d --force-recreate --build..."
+docker-compose up -d --force-recreate --build
